42d28043fbb477a11ee3e193dccef4f0b5663907
fmtabbara <fmtabbara@hotmail.co.uk>
2022-07-21 21:50:40 +0100

    new form flow

--- nym-wallet/src/pages/bonding/index.tsx
+++ nym-wallet/src/pages/bonding/index.tsx
@@ -1,4 +1,5 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
+import { BondModal } from 'src/components/Bonding/modals/BondModal';
 import { AppContext } from 'src/context/main';
 import { BondingContextProvider, useBondingContext } from '../../context';
 import { PageLayout } from '../../layouts';
@@ -7,12 +8,17 @@ import GatewayCard from './gateway';
 import MixnodeCard from './mixnode';
 
 const Bonding = () => {
+  const [showModal, setShowModal] = useState<'bond'>();
+  const { clientDetails } = useContext(AppContext);
   const { bondedMixnode, bondedGateway } = useBondingContext();
 
   // TODO display a special UI on loading state
   return (
     <PageLayout>
-      {!bondedMixnode && !bondedGateway && <BondingCard />}
+      {showModal === 'bond' && (
+        <BondModal onClose={() => setShowModal(undefined)} denom={clientDetails?.display_mix_denom || 'nym'} />
+      )}
+      {!bondedMixnode && !bondedGateway && <BondingCard onBond={() => setShowModal('bond')} />}
       {bondedMixnode && <MixnodeCard mixnode={bondedMixnode} />}
       {bondedGateway && <GatewayCard gateway={bondedGateway} />}
     </PageLayout>
